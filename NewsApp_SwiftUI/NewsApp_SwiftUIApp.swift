d0a821966122659a14f5bd964237194db017de3e
Kristel <idmaksimovak@gmail.com>
2023-05-03 21:26:55 +0300

    Initial Commit

---
+++ NewsApp_SwiftUI/NewsApp_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  NewsApp_SwiftUIApp.swift
+//  NewsApp_SwiftUI
+//
+//  Created by Kristi on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_SwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
