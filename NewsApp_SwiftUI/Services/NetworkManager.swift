5d4ec29f85421e79519c936ba386c3b40513a678
Kristel <idmaksimovak@gmail.com>
2023-05-04 10:58:37 +0300

    Handled errors in the request

--- NewsApp_SwiftUI/Services/NetworkManager.swift
+++ NewsApp_SwiftUI/Services/NetworkManager.swift
@@ -14,28 +14,35 @@ enum Link: String {
     case link = "https://newsapi.org/v2/everything?q=tesla&from=2023-04-03&sortBy=publishedAt&apiKey=37eeec7e4c0f4bba8215989f3e6cef32"
 }
 
-
 class NetworkManager {
 
     func getArticles(completion: @escaping ([Articles]) -> ()) {
         guard let url = URL(string: Link.link.rawValue) else { return }
         
-        URLSession.shared.dataTask(with: url) { (data, _, _) in
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                print("Error retrieving data: \(error)")
+                completion([])
+                return
+            }
             
-            do {
-                let posts = try JSONDecoder().decode(Post.self, from: data!)
-                DispatchQueue.main.async {
-                    completion(posts.articles)
-                }
-            } catch {
-                print("Error decoding data: \(error)")
+            if let data = data {
+                do {
+                    let posts = try JSONDecoder().decode(Post.self, from: data)
+                    DispatchQueue.main.async {
+                        completion(posts.articles)
+                    }
+                } catch {
+                    print("Error decoding data: \(error)")
 
-                DispatchQueue.main.async {
-                    completion([])
+                    DispatchQueue.main.async {
+                        completion([])
+                    }
                 }
+            } else {
+                completion([])
             }
         }
         .resume()
     }
 }
-
